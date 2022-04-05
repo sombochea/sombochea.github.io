6a18f634f0f49979a85f52d6e1233a4feaf6a72a
sombochea <7059827+sombochea@users.noreply.github.com>
2022-04-05 10:32:37 +0000

    Deploying to gh-pages from @ sombochea/sombochea.github.io@1d1e5b0fca2b3531de2970a44104952d04df773d 🚀

--- pages/helper/ImageLoader.tsx
+++
@@ -1,8 +0,0 @@
-import { ImageLoaderProps } from "next/image";
-
-const CustomImageLoader = (props: ImageLoaderProps) => {
-  const { src, width, quality } = props;
-  return `${src}?w=${width}&q=${quality || 75}`;
-};
-
-export default CustomImageLoader;
